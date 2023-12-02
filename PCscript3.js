d5811d2291b5d8c61bb9edc2b611f9320cb9875c
ZedrickFrancky <150829491+ZedrickFrancky@users.noreply.github.com>
2023-12-02 12:40:24 +0800

    Add files via upload

---
+++ PCscript3.js
@@ -0,0 +1,17 @@
+fetch('item_cold.json')
+.then(response => response.json())
+.then(data => {
+let displayOutput = "";
+for (const item of data) {
+displayOutput += `
+   <div id="itemscon">  
+   <img src="${item.images}" id="itemsimg">
+   <p id="itemsname">${item.name}</p>
+   <p id="itemsdes">${item.description}</p>
+   <p id="itemvalue">${item.price_php}</p>
+   </div>
+   
+  `;
+}
+document.querySelector("#items").innerHTML = displayOutput;
+})
