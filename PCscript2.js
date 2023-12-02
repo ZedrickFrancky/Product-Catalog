5857fdcbb42da3754f7af051bca689d5e6168842
ZedrickFrancky <150829491+ZedrickFrancky@users.noreply.github.com>
2023-12-02 21:46:14 +0800

    Add files via upload

---
+++ PCscript2.js
@@ -0,0 +1,28 @@
+fetch('item_warm.json')
+.then(response => response.json())
+.then(data => {
+let displayOutput = "";
+for (const item of data) {
+displayOutput += `
+   <div id="itemscon">  
+   <img src="${item.images}" id="itemsimg">
+   <p id="itemsname">${item.name}</p>
+   <p id="itemsdes">${item.description}</p>
+   <p id="itemvalue">${item.price_php}</p>
+   </div>
+   
+  `;
+}
+document.querySelector("#items").innerHTML = displayOutput;
+document.querySelectorAll('#itemscon').forEach(item => {
+  item.addEventListener('click', openPopup);
+});
+});
+
+function openPopup() {
+document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+document.getElementById('popup').style.display = 'none';
+}
